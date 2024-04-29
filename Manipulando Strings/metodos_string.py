f0ab06f1c510c7e9201d3a56ea77f02365707ab2
Lucas Silva <pv.lucassilva@gmail.com>
2024-04-29 18:20:03 -0300

    update "metodos"

--- Manipulando Strings/metodos_string.py
+++ Manipulando Strings/metodos_string.py
@@ -1,14 +1,17 @@
 curso = "  pYtHon "
+
 print(curso.upper()) # todas maiusculas
 print(curso.lower()) # todas minusculas
 print(curso.title()) # somente a primeira letra maisucula
 
 frase = "  Olá mundo!   " 
+
 print(frase.strip() + ".") # remove espaços esquerda e direita
 print(frase.lstrip() + ".") # remove espaços da esquerda 
 print(frase.rstrip() + ".") # remove espaços da direita
 
 curso2 = "Python"
+
 print (curso2.center(10, "#"))
 print (".".join(curso2))
 
