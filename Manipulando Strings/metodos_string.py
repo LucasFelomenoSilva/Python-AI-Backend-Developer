1cdeb23887f0fdf78a776da222931f87653a2b5d
Lucas Silva <pv.lucassilva@gmail.com>
2024-04-29 18:16:39 -0300

    uodate "metodos"

--- Manipulando Strings/metodos_string.py
+++ Manipulando Strings/metodos_string.py
@@ -1,15 +1,14 @@
 curso = "  pYtHon "
-
 print(curso.upper()) # todas maiusculas
 print(curso.lower()) # todas minusculas
 print(curso.title()) # somente a primeira letra maisucula
 
-print(curso.strip()) # remove espaços esquerda e direita
-print(curso.lstrip()) # remove espaços da esquerda 
-print(curso.rstrip()) # remove espaços da direita
+frase = "  Olá mundo!   " 
+print(frase.strip() + ".") # remove espaços esquerda e direita
+print(frase.lstrip() + ".") # remove espaços da esquerda 
+print(frase.rstrip() + ".") # remove espaços da direita
 
 curso2 = "Python"
-
 print (curso2.center(10, "#"))
 print (".".join(curso2))
 
