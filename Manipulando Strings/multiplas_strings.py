74299909acd8af7cc44eb37cb49881b386113641
Lucas Silva <pv.lucassilva@gmail.com>
2024-04-29 19:02:06 -0300

    add "multiplas_strings"

---
+++ Manipulando Strings/multiplas_strings.py
@@ -0,0 +1,22 @@
+nome = "Lucas"
+
+mensagem = f'''    
+        Olá! Meu nome é {nome}
+    Eu estou estudando Python
+        Esta linha tem diferentes recuos
+    '''
+print(mensagem)
+
+print(
+    """
+      ========= MENU ==========
+      
+           1 - Depositar
+           2 - Sacar
+           3 - Sair
+      
+      =========================
+      
+        Obrigado por usar nosso sistema! 
+    """      
+      )
