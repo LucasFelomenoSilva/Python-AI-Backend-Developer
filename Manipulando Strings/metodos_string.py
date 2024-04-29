f16332933f0901f1d7289c5a2dd6cd904b267d0a
Lucas Silva <pv.lucassilva@gmail.com>
2024-04-29 18:07:26 -0300

    add "metodos_strings"

---
+++ Manipulando Strings/metodos_string.py
@@ -0,0 +1,15 @@
+curso = "  pYtHon "
+
+print(curso.upper()) # todas maiusculas
+print(curso.lower()) # todas minusculas
+print(curso.title()) # somente a primeira letra maisucula
+
+print(curso.strip()) # remove espaços esquerda e direita
+print(curso.lstrip()) # remove espaços da esquerda 
+print(curso.rstrip()) # remove espaços da direita
+
+curso2 = "Python"
+
+print (curso2.center(10, "#"))
+print (".".join(curso2))
+
